c97cef33d96f53666c650cec34bc6575301eb71e
BobbyMoreau <bobbymoreauraquin@gmail.com>
2023-02-18 13:05:59 +0100

    Created databaseconnection

---
+++ Data/FlowersContext.cs
@@ -0,0 +1,15 @@
+
+using flowers.api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace flowers.api.Data
+{
+    public class FlowersContext : DbContext
+    {
+        public DbSet<Flower> Flowers { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public FlowersContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using flowers.api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add DB
+builder.Services.AddDbContext<FlowersContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
+});
 
 var app = builder.Build();
 
