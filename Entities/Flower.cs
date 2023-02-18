aa7625b54aa440efa720444f96b0938aa0dda53f
BobbyMoreau <bobbymoreauraquin@gmail.com>
2023-02-18 12:35:03 +0100

    Added properties

--- Entities/Flower.cs
+++ Entities/Flower.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace flowers.api.Entities
 {
     public class Flower
     {
-         public int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Color { get; set; }   
         public int Height { get; set; }
+        public int FamilyId { get; set; }
+
+        [ForeignKey("FamilyId")]
+        public Family Family { get; set; }
     }
 }
